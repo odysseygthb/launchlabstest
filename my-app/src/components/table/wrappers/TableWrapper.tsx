ee9487e585f778741cb415398ecb10a264c822cc
prystupkobohdan <prystupkobohdan@gmail.com>
2026-02-22 15:49:08 +0200

    feat: render filters

--- my-app/src/components/table/wrappers/TableWrapper.tsx
+++ my-app/src/components/table/wrappers/TableWrapper.tsx
@@ -1,21 +1,61 @@
 import { useState } from 'react';
-import { useAthletes } from '../../../hooks/useAtheletes.ts';
-import { Table } from '../organisms/Table.tsx';
+import { useAthletes } from '../../../hooks/useAtheletes';
+import { Table } from '../organisms/Table';
+import { FilterByIdWrapper } from './FilterByIdWrapper';
+import { FilterByNameWrapper } from './FilterByNameWrapper';
+import { FilterByOptionsWrapper } from './FilterByOptionsWrapper';
+import type { AthleteFilters } from '../../../types';
+import styles from './TableWrapper.module.css';
 
 const PAGE_SIZE = 25;
 
 export function TableWrapper() {
     const [page, setPage] = useState(0);
+    const [idFilter,      setIdFilter]      = useState<number | undefined>(undefined);
+    const [nameFilter,    setNameFilter]    = useState<string | undefined>(undefined);
+    const [optionFilters, setOptionFilters] = useState<AthleteFilters>({});
 
-    const { data = [] } = useAthletes({ page, pageSize: PAGE_SIZE });
+    const filters: AthleteFilters = {
+        id:      idFilter,
+        name:    nameFilter,
+        sport:   optionFilters.sport,
+        country: optionFilters.country,
+        status:  optionFilters.status,
+        gender:  optionFilters.gender,
+    };
+
+    const { data = [] } = useAthletes({ filters, page, pageSize: PAGE_SIZE });
+
+    function handleIdChange(id: number | undefined) {
+        setIdFilter(id);
+        setPage(0);
+    }
+
+    function handleNameChange(name: string | undefined) {
+        setNameFilter(name);
+        setPage(0);
+    }
+
+    function handleOptionsChange(next: AthleteFilters) {
+        setOptionFilters(next);
+        setPage(0);
+    }
 
     return (
-        <Table
-            athletes={data}
-            page={page}
-            pageSize={PAGE_SIZE}
-            onPrevPage={() => setPage(p => Math.max(0, p - 1))}
-            onNextPage={() => setPage(p => p + 1)}
-        />
+        <>
+            <div className={styles.filters}>
+                <FilterByIdWrapper      onIdChange={handleIdChange} />
+                <FilterByNameWrapper    onNameChange={handleNameChange} />
+                <FilterByOptionsWrapper onFiltersChange={handleOptionsChange} />
+            </div>
+
+            <Table
+                athletes={data}
+                page={page}
+                pageSize={PAGE_SIZE}
+                onPrevPage={() => setPage(p => Math.max(0, p - 1))}
+                onNextPage={() => setPage(p => p + 1)}
+            />
+        </>
     );
 }
